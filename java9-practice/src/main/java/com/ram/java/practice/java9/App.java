74799289e1b658c7c1002869e149d68715e1590e
Ramesh Udatha <udara01@ca.com>
2019-01-08 19:40:33 +0530

    Initial project using Maven Commands

---
+++ java9-practice/src/main/java/com/ram/java/practice/java9/App.java
@@ -0,0 +1,13 @@
+package com.ram.java.practice.java9;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
